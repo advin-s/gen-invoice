1012a53d54dead538e9507cfcdda37afd62e04ad
Adith Vinod <adith.v@signdesk.com>
2025-01-01 15:48:45 +0530

    card component

---
+++ src/lib/Card.js
@@ -0,0 +1,19 @@
+
+
+const Card = (props) =>{
+const Heading = ({heading,icon}) =>{
+  return <div className="flex items-center gap-x-3">
+    <div className="h-14 w-14 rounded-full bg-theme-secondary grid place-items-center">
+      {icon}
+    </div>
+    <p className="mb-0 text-2xl font-medium capitalize">{heading}</p>
+  </div>
+}
+
+return <div>
+  <Heading {...props}/>
+</div>
+
+}
+
+export default Card
