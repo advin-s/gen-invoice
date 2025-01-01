7dc07f0bf200a9e44bdd93533796eb94b0371789
Adith Vinod <adith.v@signdesk.com>
2025-01-01 19:49:40 +0530

    created heading component

---
+++ src/lib/Heading.js
@@ -0,0 +1,11 @@
+const Heading = ({heading,icon}) =>{
+  console.log(heading,'---------');
+  return <div className="flex items-center gap-x-3">
+    <div className="h-14 w-14 rounded-full bg-theme-secondary grid place-items-center">
+      {icon}
+    </div>
+    <p className="mb-0 text-2xl font-medium capitalize">{heading}</p>
+  </div>
+}
+
+export default Heading
